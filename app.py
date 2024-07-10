42e88fed24d724afcd13e3858a565b0618a861b8
Aziz Prabowo <azizprabowo128@gmail.com>
2024-07-10 21:41:07 +0700

    add new examples

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ descriptions = "Analisis Sentimen Bahasa Jawa adalah aplikasi yang dapat mempred
 article = """#### Note: Refresh halaman jika stuck di proses prediksi."""
 examples = [["Aku tresno banget karo koe mas."], 
     ["Mbok ojo dadi wong sing nganyeli."], 
-    ["Teles kebes netes eluh neng dadaku."]]
+    ["Teles kebes netes eluh neng dadaku."],
+    ["Aku sayang karo koe beb, tapi ngapusi"],
+    ["Sedih aku. Lagi mangan iwakku malah dicolong pitek"]]
 
 demo = gr.load(name=name, title=title, description=descriptions, article=article, examples=examples, allow_flagging="auto")
 
