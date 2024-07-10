c5fdc1f1242fe5752545bff632dc04add1e58f5d
Aziz Prabowo <azizprabowo128@gmail.com>
2024-07-10 21:46:28 +0700

    update title & change the interface

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import gradio as gr
 
 name = "models/azizp128/jawa-sentiment-analysis-indobert"
-title = "Analisis Sentimen Bahasa Jawa"
-descriptions = "Analisis Sentimen Bahasa Jawa adalah aplikasi yang dapat memprediksi sentimen positif atau negatif dari kalimat berbahasa Jawa Ngoko Lugu."
+title = "Analisis Sentimen Bahasa Jawa Ngoko Lugu"
+descriptions = "Analisis Sentimen Bahasa Jawa adalah aplikasi yang dapat memprediksi sentimen positif atau negatif dari teks kalimat berbahasa Jawa Ngoko Lugu."
 article = """#### Note: Refresh halaman jika stuck di proses prediksi."""
 examples = [["Aku tresno banget karo koe mas."], 
     ["Mbok ojo dadi wong sing nganyeli."], 
@@ -10,7 +10,7 @@ examples = [["Aku tresno banget karo koe mas."],
     ["Aku sayang karo koe beb, tapi ngapusi"],
     ["Sedih aku. Lagi mangan iwakku malah dicolong pitek"]]
 
-demo = gr.load(name=name, title=title, description=descriptions, article=article, examples=examples, allow_flagging="auto")
+demo = gr.Interface.load(name=name, title=title, description=descriptions, article=article, examples=examples, allow_flagging="auto")
 
 if __name__ == "__main__":
     demo.launch()
