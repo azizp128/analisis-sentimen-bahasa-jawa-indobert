de76178c5b8c8f48f210f991c2b9e984a7bac50d
Aziz Prabowo <azizprabowo128@gmail.com>
2024-07-13 23:07:37 +0700

    updated code

--- app.py
+++ app.py
@@ -1,45 +1,16 @@
 import gradio as gr
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
-import torch
 
-# Load the model and tokenizer
-model_name = "models/azizp128/jawa-sentiment-analysis-indobert"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
-
-# Prediction function
-def predict(text):
-    inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
-    with torch.no_grad():
-        outputs = model(**inputs)
-    logits = outputs.logits
-    probabilities = torch.nn.functional.softmax(logits, dim=1)
-    predicted_class_id = probabilities.argmax().item()
-    labels = {0: "negative", 1: "positive"}
-    return labels[predicted_class_id]
-
-# Gradio interface setup
+name = "models/azizp128/jawa-sentiment-analysis-indobert"
 title = "Analisis Sentimen Bahasa Jawa Ngoko Lugu"
-description = "Analisis Sentimen Bahasa Jawa adalah aplikasi yang dapat memprediksi sentimen positif atau negatif dari teks kalimat berbahasa Jawa Ngoko Lugu."
-article = "#### Note: Refresh halaman jika stuck di proses prediksi."
-examples = [
-    ["Aku tresno banget karo koe mas."], 
+descriptions = "Analisis Sentimen Bahasa Jawa adalah aplikasi yang dapat memprediksi sentimen positif atau negatif dari teks kalimat berbahasa Jawa Ngoko Lugu."
+article = """#### Note: Refresh halaman jika stuck di proses prediksi."""
+examples = [["Aku tresno banget karo koe mas."], 
     ["Mbok ojo dadi wong sing nganyeli."], 
     ["Teles kebes netes eluh neng dadaku."],
     ["Aku sayang karo koe beb, tapi ngapusi"],
-    ["Sedih aku. Lagi mangan iwakku malah dicolong pitek"]
-]
+    ["Sedih aku. Lagi mangan iwakku malah dicolong pitek"]]
 
-demo = gr.Interface(
-    fn=predict,
-    inputs="textbox",
-    outputs="label",
-    title=title,
-    description=description,
-    article=article,
-    examples=examples,
-    allow_flagging="auto"
-)
+demo = gr.Interface.load(name=name, title=title, description=descriptions, article=article, examples=examples, allow_flagging="auto")
 
 if __name__ == "__main__":
     demo.launch()
