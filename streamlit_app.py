1a444e70d2d983e7c443baecb50f5936890e9072
Aziz Prabowo <azizprabowo128@gmail.com>
2024-12-16 01:33:06 +0700

    added examples

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,10 @@ device = 0 if torch.cuda.is_available() else -1
 pipe = pipeline("sentiment-analysis",
                 model="azizp128/javanese-sentiment-analysis-indobert")
 
+# Initialize session state for result
+if "result" not in st.session_state:
+    st.session_state.result = None
+
 st.markdown(
     """
     <h1 style="text-align: center; font-family: sans-serif;">
@@ -20,16 +24,32 @@ st.markdown(
 )
 
 # Description
-st.write("Model analisis sentimen yang dapat memprediksi sentimen positif atau negatif dari teks berbahasa Jawa Ngoko Lugu.")
+st.write("Model analisis sentimen berbasis IndoBERT yang dapat memprediksi sentimen positif atau negatif dari teks berbahasa Jawa Ngoko Lugu.")
 
 # User input
-user_input = st.text_input("Input:", placeholder="Masukkan teks")
+user_input = st.text_input("Input", placeholder="Masukkan teks")
+
+# Examples
+options = ["Aku tresno banget karo koe mas.",
+           "Mbok ojo dadi wong sing nganyeli.",
+           "Teles kebes netes eluh neng dadaku.",
+           "Aku sayang karo koe beb, tapi ngapusi",
+           "Sedih aku. Lagi mangan iwakku malah dicolong pitek"]
+selection = st.segmented_control(
+    "Examples", options
+)
 
 # Prediction
+if selection:
+    st.session_state.result = pipe(selection)
+
 if user_input:
-    result = pipe(user_input)
-    for res in result:
-        st.write("Prediksi:")
-        st.success(res['label'])
-        st.write("Skor:")
-        st.success(res['score'])
+    st.session_state.result = pipe(user_input)
+
+# Display results if available
+if st.session_state.result:
+    for res in st.session_state.result:
+        st.write("Prediction")
+        st.success(res['label'].capitalize())
+        st.write("Score")
+        st.success(round(res['score'], 4))
